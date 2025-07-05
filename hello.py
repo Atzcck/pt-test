63b2c37c5106b025d00293499a830ca97a3b2a92
Atzcck <alptazecicek@gmail.com>
2025-07-05 08:48:02 +0300

    Initialize PyQt6 application with basic window setup

--- hello.py
+++ hello.py
@@ -1,1 +1,16 @@
-print("helloworld")
+import sys
+from PyQt6.QtWidgets import QApplication, QLabel, QLineEdit, QPushButton, QVBoxLayout, QWidget, QTextEdit
+from PyQt6.QtGui import QIcon
+
+class MyApp(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        
+app = QApplication(sys.argv)
+
+window = MyApp()
+window.show()
+
+
+sys.exit(app.exec())
